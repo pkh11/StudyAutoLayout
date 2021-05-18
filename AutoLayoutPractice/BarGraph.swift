c71ce5e0dc100aa5c9ede76c5bcff1b2175e8178
pkh <toby369369@naver.com>
2021-05-18 18:01:19 +0900

    add barGraph

---
+++ AutoLayoutPractice/BarGraph.swift
@@ -0,0 +1,8 @@
+//
+//  BarGraph.swift
+//  AutoLayoutPractice
+//
+//  Created by 박균호 on 2021/05/18.
+//
+
+import Foundation
--- AutoLayoutPractice/MessagingBubble/MessagingViewController.swift
+++ AutoLayoutPractice/MessagingBubble/MessagingViewController.swift
@@ -27,6 +27,7 @@ class MessagingViewController: UIViewController {
         super.viewDidLoad()
 
         tableView.dataSource = self
+        messageFiled.delegate = self
         
         NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { (notification) in
             guard let userInfo = notification.userInfo else { return }
@@ -90,4 +91,47 @@ extension MessagingViewController: UITableViewDataSource {
     }
     
     
+}
+
+extension MessagingViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+            // replacementString : 방금 입력된 문자 하나, 붙여넣기 시에는 붙여넣어진 문자열 전체
+            // return -> 텍스트가 바뀌어야 한다면 true, 아니라면 false
+            // 이 메소드 내에서 textField.text는 현재 입력된 string이 붙기 전의 string
+            
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .decimal // 1,000,000
+            formatter.locale = Locale.current
+            formatter.maximumFractionDigits = 0 // 허용하는 소숫점 자리수
+            
+            // formatter.groupingSeparator // .decimal -> ,
+            
+            if let removeAllSeprator = textField.text?.replacingOccurrences(of: formatter.groupingSeparator, with: ""){
+                var beforeForemattedString = removeAllSeprator + string
+                print("removeAllSeprator : \(removeAllSeprator) / string: \(string)")
+                if formatter.number(from: string) != nil {
+                    if let formattedNumber = formatter.number(from: beforeForemattedString),
+                       let formattedString = formatter.string(from: formattedNumber){
+                        textField.text = formattedString
+                        return false
+                    }
+                }else{ // 숫자가 아닐 때
+                    if string == "" { // 백스페이스일때
+                        let lastIndex = beforeForemattedString.index(beforeForemattedString.endIndex, offsetBy: -1)
+                        beforeForemattedString = String(beforeForemattedString[..<lastIndex])
+                        print("backspace : \(beforeForemattedString)")
+                        if let formattedNumber = formatter.number(from: beforeForemattedString),
+                           let formattedString = formatter.string(from: formattedNumber){
+                            textField.text = formattedString
+                            return false
+                        }
+                    }else{ // 문자일 때
+                        return false
+                    }
+                }
+
+            }
+
+            return true
+        }
 }
